b2aec1dfca6a3946b5cdf90b5bdac8af845bf4bd
Softtech4niyi <softtech4niyi@gmail.com>
2025-03-25 08:51:43 +0100

    Hellow world

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+public static void main(String[] args){
+    //int first_num = 40;
+    //int second_num = 3;
+    //int mul = first_num * second_num;
+
+   // if(mul > 400){
+    //    System.out.print("Niyi");
+   // }
+   // else{
+    //    System.out.print("Ariyo");
+   // }
+   for(int x = 1; x < 10; x++){
+   System.out.println(x + "." + "Niyi");
+   }
+    
+}
+}
